41c40bb0d5a19d62b3b448be83155645219eadf9
Giridhar <bhujbalgiridhar@gmail.com>
2024-06-05 22:41:52 +0530

    Adding new Info Method

--- src/com/giridhar/Demo.java
+++ src/com/giridhar/Demo.java
@@ -2,11 +2,19 @@ package com.giridhar;
 
 public class Demo {
 
+	public void info() {
+		int a = 7;
+		for (int i = 1; i <= 10; i++) {
+			System.out.print(a * i + " ");
+		}
+	}
+
 	public static void main(String[] args) {
 
 		System.out.println("i am from demo class");
 		System.out.println("hello giridhar");
-
+		Demo demo = new Demo();
+		demo.info();
 
 	}
 
