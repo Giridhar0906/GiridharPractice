a34e09565efd37f178182fac8d1757f6f1e2ffd6
Giridhar <bhujbalgiridhar@gmail.com>
2024-06-05 20:47:15 +0530

    Adding Just Practice

---
+++ src/com/giridhar/Demo.java
@@ -0,0 +1,13 @@
+package com.giridhar;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		System.out.println("i am from demo class");
+		System.out.println("hello giridhar");
+
+
+	}
+
+}
