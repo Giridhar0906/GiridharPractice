821972af59802bfdf4642ca4b2580b26092ed8a8
Giridhar <bhujbalgiridhar@gmail.com>
2024-08-14 10:53:08 +0530

    Revert "add infoDisplay method"
    This reverts commit 18c5a969d79f034ac1f12e7f1a09fa08b39968c5.

--- src/com/giridhar/Demo.java
+++ src/com/giridhar/Demo.java
@@ -9,24 +9,12 @@ public class Demo {
 		}
 	}
 
-public void infoDisplay() {
-System.out.print("hi i am giridhar");
-
-		int b =11;
-		for (int j = 1; j <= 10; j++) {
-			System.out.print(b * j + " ");
-		}
-	}
-
-
 	public static void main(String[] args) {
 
 		System.out.println("i am from demo class");
 		System.out.println("hello giridhar");
 		Demo demo = new Demo();
 		demo.info();
-demo.infoDisplay();
-
 
 	}
 
