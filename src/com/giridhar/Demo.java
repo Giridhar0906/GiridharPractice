9e6a463a87db0d04f7ba087c24a39132bc187ad0
Giridhar <bhujbalgiridhar@gmail.com>
2024-08-14 10:25:27 +0530

    add valuve of a

--- src/com/giridhar/Demo.java
+++ src/com/giridhar/Demo.java
@@ -3,7 +3,7 @@ package com.giridhar;
 public class Demo {
 
 	public void info() {
-		int a = 9;
+		int a =11;
 		for (int i = 1; i <= 10; i++) {
 			System.out.print(a * i + " ");
 		}
@@ -11,7 +11,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 
-		System.out.println("hello giridhar");
+		System.out.println("hello i am giridhar");
 		Demo demo = new Demo();
 		demo.info();
 
