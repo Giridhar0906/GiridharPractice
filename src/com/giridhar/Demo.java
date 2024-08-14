facf88145f70d718e8c5087d4e1152ed8f85c519
Giridhar <bhujbalgiridhar@gmail.com>
2024-08-14 10:29:09 +0530

    Revert "add valuve of a"
    This reverts commit 9e6a463a87db0d04f7ba087c24a39132bc187ad0.

--- src/com/giridhar/Demo.java
+++ src/com/giridhar/Demo.java
@@ -3,7 +3,7 @@ package com.giridhar;
 public class Demo {
 
 	public void info() {
-		int a =11;
+		int a = 9;
 		for (int i = 1; i <= 10; i++) {
 			System.out.print(a * i + " ");
 		}
@@ -11,7 +11,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 
-		System.out.println("hello i am giridhar");
+		System.out.println("hello giridhar");
 		Demo demo = new Demo();
 		demo.info();
 
