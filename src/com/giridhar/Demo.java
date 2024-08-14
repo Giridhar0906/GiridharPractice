6be9b4010f1a803c791ff5fbc832bb6d8023a210
Giridhar <bhujbalgiridhar@gmail.com>
2024-08-14 10:32:34 +0530

    Revert "changed value of variable a"
    This reverts commit 50c401a7370b08a77d52575e40c3ebe800583a2b.

--- src/com/giridhar/Demo.java
+++ src/com/giridhar/Demo.java
@@ -3,7 +3,7 @@ package com.giridhar;
 public class Demo {
 
 	public void info() {
-		int a = 9;
+		int a = 7;
 		for (int i = 1; i <= 10; i++) {
 			System.out.print(a * i + " ");
 		}
@@ -11,6 +11,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 
+		System.out.println("i am from demo class");
 		System.out.println("hello giridhar");
 		Demo demo = new Demo();
 		demo.info();
